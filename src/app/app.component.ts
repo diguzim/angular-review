24e582f2b98e54799baffd0ab56c5ccd75bc276b
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2025-02-20 20:29:25 -0300

    Moved counter to components folder, added randomOutputHandler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CounterComponent } from './counter/counter.component';
-import { PostComponent } from './components/post/post.component';
 import { BlogService } from './services/blog.service';
-import { Post } from './models/interfaces/post.interface';
+import { CounterComponent } from './components/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   providers: [BlogService],
-  imports: [RouterOutlet, CounterComponent, PostComponent],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-review';
   count = signal(0);
+
+  randomOutputHandler(value: number) {
+    console.log('Random output:', value);
+  }
 }
--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, effect, input, model } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  input,
+  model,
+  output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -10,6 +17,7 @@ export class CounterComponent {
   label = input('MyCounter');
   count = model(0);
   isEven = computed(() => this.count() % 2 === 0);
+  randomOutput = output<number>();
 
   private loggingEffect = effect(() => {
     console.log(`Current value of ${this.label} is ${this.count()}`);
@@ -17,5 +25,9 @@ export class CounterComponent {
 
   increment() {
     this.count.update((value) => value + 1);
+  }
+
+  randomOutputHandler() {
+    this.randomOutput.emit(Math.random());
   }
 }
